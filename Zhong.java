109108cb5ce99eb1511a3808eb11116fdf65ac74
jintianyan <1436335453@qq.com>
2020-03-16 16:54:38 +0800

    钟

--- Zhong.java
+++ Zhong.java
@@ -5,5 +5,6 @@ public class Zhong {
         System.out.println("祖安七年 父母双全");
         System.out.println("祖安大舞台，有妈你就来");
         System.out.println("你的房间怎么这么乱呀 你爸妈呢？ 你爸妈去哪了？ 难道说你爸妈.......");
+        System.out.println("***");
     }
 }
