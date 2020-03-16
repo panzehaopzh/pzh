11c5cf4622c9c2045a88305454f24f5a9bf582c9
qq919644223 <919644223@qq.com>
2020-03-16 16:49:45 +0800

    Merge branch 'master' of E:\test_p with conflicts.

--- Zhong.java
+++ Zhong.java
@@ -2,8 +2,6 @@ public class Zhong {
     public static void main(String[] args) {
         System.out.println("吃尼玛");
         System.out.println("我可是祖安的  有妈你就接收挑战");
-        System.out.println("祖安七年 父母双全");
-        System.out.println("祖安大舞台，有妈你就来");
-        System.out.println("你的房间怎么这么乱呀 你爸妈呢？ 你爸妈去哪了？ 难道说你爸妈.......");
+        System.out.println("祖安七年 父母双全    ");
     }
 }
